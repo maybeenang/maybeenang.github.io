b74e1b76710a52a1b5e4c6305e2991b799a3cb10
Muhammad Elang Permadani <muhammad.120140194@student.itera.ac.id>
2025-11-30 21:39:39 +0700

    footer cp

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div
       className={cn(
-        'bg-background-secondary relative flex h-16 items-center justify-center text-center text-sm text-text-secondary',
+        'bg-background-secondary relative flex h-16 items-center justify-between px-2 text-center text-sm text-text-secondary',
         'before:absolute before:top-0 before:-left-[100vw] before:h-px before:w-[200vw] before:bg-border',
         'after:absolute after:bottom-0 after:-left-[100vw] after:h-px after:w-[200vw] after:bg-border',
       )}
@@ -12,14 +12,25 @@ const Footer = () => {
       <h1>
         © {new Date().getFullYear()} Built with 😂 by{' '}
         <a
-          className="cursor-pointer hover:underline"
+          className="cursor-pointer underline hover:text-text"
           href="https://github.com/maybeenang"
           target="_blank"
           rel="noopener noreferrer"
         >
           maybeenang
         </a>
-        .
+      </h1>
+      <h1>
+        Inspired by{' '}
+        <a
+          className="cursor-pointer underline hover:text-text"
+          href="https://tailwindcss.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Tailwind CSS{' '}
+        </a>
+        Landing page
       </h1>
     </div>
   );
